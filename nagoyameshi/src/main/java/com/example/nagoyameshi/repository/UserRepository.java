d21ba80d3b9720b851cdc618d33cb59b05fb3b02
sato-at <fukurou0art@gmail.com>
2024-08-30 16:08:02 +0900

    15,16章ログイン機能を作ろう

--- nagoyameshi/src/main/java/com/example/nagoyameshi/repository/UserRepository.java
+++ nagoyameshi/src/main/java/com/example/nagoyameshi/repository/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.nagoyameshi.entity.User;
 
+
 public interface UserRepository extends JpaRepository<User, Integer> {
-	
+  public User findByEmail(String email);
 }
