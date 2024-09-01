0b2d8550f4d55aec79d707ef6890ac3e0bc1ef6e
sato-at <fukurou0art@gmail.com>
2024-09-01 19:03:24 +0900

    17~１９章
    17章 共通のHTML要素を部品化しよう
    18章 会員登録ページを作ろう（フォームクラス編）
    19章 会員登録ページを作ろう（コントローラ・ビュー編）

--- nagoyameshi/src/main/java/com/example/nagoyameshi/controller/AuthController.java
+++ nagoyameshi/src/main/java/com/example/nagoyameshi/controller/AuthController.java
@@ -1,7 +1,10 @@
 package com.example.nagoyameshi.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.nagoyameshi.form.SignupForm;
 
 @Controller
 public class AuthController {
@@ -10,4 +13,9 @@ public class AuthController {
     return "auth/login";
   }
 
+  @GetMapping("/signup")
+  public String signup(Model model) {
+    model.addAttribute("signupForm", new SignupForm());
+    return "auth/signup";
+  }
 }
